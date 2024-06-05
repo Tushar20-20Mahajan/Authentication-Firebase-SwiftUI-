2951c80534b3bc9b1b5b9eaa16857d89237e3153
Tushar20-20Mahajan <tusharmahajan080@gmail.com>
2024-06-05 14:07:15 +0530

    Initial Commit

---
+++ Authentication+Firebase/Authentication_FirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Authentication_FirebaseApp.swift
+//  Authentication+Firebase
+//
+//  Created by Sunidhi Ratra on 05/06/24.
+//
+
+import SwiftUI
+
+@main
+struct Authentication_FirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
